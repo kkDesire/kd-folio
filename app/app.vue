1bc39db3569e7b08b54d2e6696a3f2a011a3a6d7
Desire KOUASSI <kouamdesire90@gmail.com>
2025-09-05 17:45:35 +0000

    Refactor code structure for improved readability and maintainability

--- app/app.vue
+++ app/app.vue
@@ -1,6 +1,39 @@
+<script setup lang="ts">
+const { locale } = useI18n()
+const colorMode = useColorMode()
+const color = computed(() => colorMode.value === 'dark' ? '#020618' : 'white')
+
+useHead({
+  meta: [
+    { charset: 'utf-8' },
+    { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+    { name: 'theme-color', content: color.value },
+  ],
+  link: [
+    { rel: 'icon', href: '/favicon.ico' },
+  ],
+  htmlAttrs: {
+    lang: locale.value || 'en',
+  },
+})
+
+useSeoMeta({
+  titleTemplate: '%s - KD Folio',
+  ogImage: '/assets/portfolio-light.png',
+  twitterImage: '/assets/portfolio-light.png',
+  twitterCard: 'summary_large_image',
+})
+</script>
+
 <template>
-  <div>
-    <NuxtRouteAnnouncer />
-    <NuxtWelcome />
-  </div>
+  <UApp>
+    <NuxtLayout>
+      <UMain class="relative">
+        <NuxtPage />
+        <div class="fixed bottom-0 right-0 m-5">
+          <LanguageSwitcher />
+        </div>
+      </UMain>
+    </NuxtLayout>
+  </UApp>
 </template>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,36 @@
-// https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
-  modules: ['@nuxt/content', '@nuxt/image']
+  modules: [
+    '@nuxt/ui',
+    '@nuxt/content',
+    '@nuxtjs/i18n',
+    '@nuxt/image',
+    '@vueuse/nuxt',
+    'nuxt-og-image',
+    'motion-v/nuxt',
+  ],
+  css: ['~/assets/css/main.css'],
+  i18n: {
+    locales: [
+      {
+        code: 'en',
+        file: 'en-US.ts',
+      },
+      {
+        code: 'fr',
+        file: 'fr-FR.ts',
+      },
+    ],
+    defaultLocale: 'fr',
+  },
+
+  nitro: {
+    prerender: {
+      routes: [
+        '/',
+      ],
+      crawlLinks: true,
+    },
+  },
 })
